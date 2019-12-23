4855a7e84a29d3c57b8881dd5143032b7b8ff7f9
chaoli <chao.li@usask.ca>
2019-12-23 16:25:38 +0800

    修改 界面，添加button 跳转展示界面

--- controller.js
+++ controller.js
@@ -11,8 +11,9 @@ module.exports={
 
         form.encoding = 'utf-8';
 
-        form.uploadDir = "/Users/lichao/Desktop/uploadFile-with-progress-bar-/my/dir";  //为什么需要使用绝对路径？？？？？ 而不是上课的时候的相对路径
+        form.uploadDir = "./my/dir";  //为什么需要使用绝对路径？？？？？ 而不是上课的时候的相对路径  会了，要加点
 
+        // /Users/lichao/Desktop/uploadFile-with-progress-bar-/my/dir
         //设定上传最大值  2G
         form.maxFileSize = 20000 * 1024 * 1024;
 
